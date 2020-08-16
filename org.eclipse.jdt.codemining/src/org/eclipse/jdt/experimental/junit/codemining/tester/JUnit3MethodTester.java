86b76fe66d3799dc7ac15546c9c8e56402dd26cb
Gayan Perera <gayanper@gmail.com>
2020-08-16 14:51:47 +0200

    Fix #106: Check for JUnit3 base class.

--- org.eclipse.jdt.codemining/src/org/eclipse/jdt/experimental/junit/codemining/tester/JUnit3MethodTester.java
+++ org.eclipse.jdt.codemining/src/org/eclipse/jdt/experimental/junit/codemining/tester/JUnit3MethodTester.java
@@ -1,6 +1,12 @@
 package org.eclipse.jdt.experimental.junit.codemining.tester;
 
+import java.util.stream.Stream;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.experimental.JavaCodeMiningPlugin;
 
 public class JUnit3MethodTester implements IJUnitMethodTester {
 
@@ -8,7 +14,17 @@ public class JUnit3MethodTester implements IJUnitMethodTester {
 
 	@Override
 	public boolean isTestMethod(IMethod method) {
-		return IJUnitMethodTester.isMethod(method, true) && method.getElementName().startsWith("test");
+		return IJUnitMethodTester.isMethod(method, true) && isTestClass(method.getDeclaringType())
+				&& method.getElementName().startsWith("test");
 	}
 
+	private boolean isTestClass(IType type) {
+		try {
+			return Stream.of(type.newSupertypeHierarchy(new NullProgressMonitor()).getAllSupertypes(type))
+					.filter(t -> "junit.framework.TestCase".equals(t.getFullyQualifiedName())).findFirst().isPresent();
+		} catch (JavaModelException e) {
+			JavaCodeMiningPlugin.getDefault().getLog().error("isTestClass check for JUnit3 failed.", e);
+			return false;
+		}
+	}
 }
